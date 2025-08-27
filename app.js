1f53a1ad7e5b717642e8f5e16477452b01cd4651
xumeinv <137912973+xumeinv@users.noreply.github.com>
2025-08-27 18:42:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+App({
+  onLaunch() {
+    // 小程序启动时执行
+    console.log('小程序启动成功');
+  },
+  globalData: {
+    // 全局数据
+    cozeApiUrl: 'https://psychology-generate-180919-4-1374892393.sh.run.tcloudbase.com'
+  }
+}) 
